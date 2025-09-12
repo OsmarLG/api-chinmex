de1f10344d8cb21ac427c32ccddf1209ba6f924f
Osmar Alejandro Liera Gomez <osmar2001lg@gmail.com>
2025-09-11 21:08:36 -0700

    feat: :sparkles: Telesope & Scramble
    Agregue las herramientas de Telescope para el monitoreo de la app y Scramble para la documentacion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider; 
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +27,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar el esquema HTTPS
+        URL::forceScheme('https');
+
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->components->securitySchemes['bearer'] = SecurityScheme::http('bearer');
+
+                $openApi->security[] = new SecurityRequirement([
+                    'bearer' => [],
+                ]);
+            })
+            ->routes(function (Route $route) {
+                return Str::contains($route->uri(), 'api*');
+            })
+            ->expose(
+                ui: '/docs/api',
+                document: '/docs/openapi.json',
+            );
     }
 }
